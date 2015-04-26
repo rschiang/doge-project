90b4e75173ebc58e0ca39c6476896a7cb48b5f09
Poren Chiang <ren.chiang@gmail.com>
2015-04-26 16:24:57 +0800

    Don’t let text overlap

--- doge.py
+++ doge.py
@@ -18,6 +18,11 @@ doge_colors = ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
     '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a',
     '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548']
 
+def has_overlap(rect_a, rect_b):
+    x1, y1, w1, h1 = rect_a
+    x2, y2, w2, h2 = rect_b
+    return not ((x1 + w1) < x2 or (x2 + w2) < x1 or (y1 + h1) < y2 or (y2 + h2) < y1)
+
 def dogify(keywords):
     sentences = []
     for keyword in keywords:
@@ -32,12 +37,18 @@ def generate(keywords, slug=None):
         draw.text_antialias = True
         with Image(filename='assets/doge.jpg') as source:
             with source.convert('png') as doge:
+                scene = [(222, 75, 144, 144)]   # Doge face
                 for sentence in sentences:
                     draw.fill_color = Color(random.choice(doge_colors))
-                    draw.font_size = random.randint(24, 48)
-                    metrics = draw.get_font_metrics(doge, sentence)
-                    x = random.randint(0, int(doge.width - metrics.text_width))
-                    y = random.randint(0, int(doge.height - metrics.text_height))
+                    for tries in range(10):
+                        draw.font_size = random.randint(24, 48)
+                        metrics = draw.get_font_metrics(doge, sentence)
+                        x = random.randint(0, int(doge.width - metrics.text_width))
+                        y = random.randint(0, int(doge.height - metrics.text_height))
+                        rect = (x, y, int(metrics.text_width), int(metrics.text_height))
+                        if not any(has_overlap(rect, i) for i in scene):
+                            scene.append(rect)
+                            break
                     draw.text(x, y, sentence)
                 draw.draw(doge)
                 timestamp = hashlib.sha1(datetime.now().isoformat(' ').encode()).hexdigest()
